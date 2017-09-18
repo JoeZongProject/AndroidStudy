88cafe66f9f13d2b1b95af9ddc46721a0c9da814
zongdongdong <785437085@qq.com>
2017-09-18 17:45:13 +0800

    update

---
+++ app/src/main/java/com/joe/study/androidstudy/view/home/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.joe.study.androidstudy.view.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.joe.study.androidstudy.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
